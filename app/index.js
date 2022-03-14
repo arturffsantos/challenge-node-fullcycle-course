a7a5cb8c79e485b95d5e1200da65a4fe41d3f5c3
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2022-03-14 11:39:40 -0300

    list people's name

--- app/index.js
+++ app/index.js
@@ -2,8 +2,51 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>');
+const mysql = require('promise-mysql');
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'challenge'
+};
+
+app.get('/', async (req, res) => {
+    let connection = null;
+    try{
+        connection = await mysql.createConnection(config);
+        const sql = 'SELECT name FROM people';
+        const names = await connection.query(sql);
+        
+        res.write('<h1>Full Cycle Rocks!</h1>');
+        res.write('<br/><br/><ul>');
+        names.forEach(item => res.write(`<li>${item.name}</li>`));
+        res.write('</ul>');
+
+        res.end();
+    }catch(e){
+        res.send(`ocorreu um erro: ${e}`);
+    }finally{
+        if(connection) {
+            connection.end();
+        }
+    }
+});
+
+app.post('/:name', async (req, res) => {
+    let connection = null;
+    try{
+        const {name} = req.params;
+        connection = await mysql.createConnection(config);
+        const sql = `INSERT INTO people(name) VALUES('${name}')`;
+        await connection.query(sql);
+        res.send('adicionado');
+    }catch(e){
+        res.send(`ocorreu um erro: ${e}`);
+    }finally{
+        if(connection) {
+            connection.end();
+        }
+    }
 });
 
 app.listen(port, () => {
